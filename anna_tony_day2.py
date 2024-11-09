9923cf80497d434be336a831d383480fcae2dd35
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-11-09 22:22:55 +0530

    Add files via upload

---
+++ anna_tony_day2.py
@@ -0,0 +1,6 @@
+#author Anna Tony
+#date 09-11-2024
+number1=int(input("Enter an integer number: "))
+number2=float(input("Enter a decimal number: "))
+print('Integer number is ',number1)
+print('Decimal number is ',number2)
