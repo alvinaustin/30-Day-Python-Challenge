aa29e18461ce9e8f7d5aabaea295e160c1466335
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-09 20:54:44 +0530

    Add files via upload

---
+++ aron_day2.py
@@ -0,0 +1,8 @@
+"""
+Author:Aron Bobby Daniel
+Date:09-11-2024
+"""
+number_1=int(input("Enter a integer number:"))
+number_2=float(input("Enter a decimal number:"))
+print(number_1,"-This is a integer number.")
+print(number_2,"-This is a decimal number.")
