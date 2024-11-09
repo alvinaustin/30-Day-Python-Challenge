75174a81a6359ffefeec74d02146db720d65294d
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-09 22:55:25 +0530

    Add files via upload

---
+++ annuday2.py
@@ -0,0 +1,8 @@
+'''
+author name:Annu James
+date:09/11/24
+'''
+num1=int(input('enter an integer number'))
+print('integer number is:',num1)
+num2=float(input('enter a decimal number'))
+print('decimal number is:',num2)
