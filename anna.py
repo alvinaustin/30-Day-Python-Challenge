88097716fa531e6bdc2321f41bfd00982307c8ea
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-09 20:39:20 +0530

    Add files via upload

---
+++ anna.py
@@ -0,0 +1,9 @@
+"""
+Author: ANNA MARIYA SHIBU
+Created on : 9/11/2024
+
+"""
+num1=int(input("Enter a number: "))
+num2=float(input("Enter a number: "))
+print((num1),"is a intiger")
+print((num2),"is a decimal")
