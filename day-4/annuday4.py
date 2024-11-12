2461ea6dd18e6061f52665be681ed49b01c9921e
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-12 22:26:35 +0530

    Add files via upload

---
+++ day-4/annuday4.py
@@ -0,0 +1,20 @@
+"""
+author name:Annu James
+date:12-11-24
+"""
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum=num1+num2
+print(sum)
+sub=num1-num2
+print(sub)
+mul=num1*num2
+print(mul)
+division=num1/num2
+print(division)
+floor_division=num1//num2
+print(floor_division)
+mod=num1%num2
+print(mod)
+exponent=num1**num2
+print(exponent)
