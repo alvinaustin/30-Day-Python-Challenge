0dbe16d006064cd211972aa77cc93c495444c720
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-12 19:28:04 +0530

    Add files via upload

---
+++ day-4/annamariya_day4.py
@@ -0,0 +1,23 @@
+"""
+Author: AnnA Mariya Shibu
+Created On : 12/11/2024
+"""
+
+
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=num1+num2
+print(sum)
+sub=num1-num2
+print(sub)
+mul=num1*num2
+print(mul)
+div=num1/num2
+print(div)
+floordiv=num1//num2
+print(floordiv)
+mod=num1%num2
+print(mod)
+exponent=num1**num2
+print(exponent)
