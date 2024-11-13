2ca0ab8ae1b1179ad333a3b84d8c6acc57d2de9c
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2024-11-13 21:14:34 +0530

    Add files via upload

---
+++ day-4/anna_tony_day4.py
@@ -0,0 +1,19 @@
+#author name:Anna Tony
+#date:12-11-24
+#arithmetic operations in Python
+num1=int(input("Enter the first number : "))
+num2=int(input("Enter the second number : "))
+sum=num1+num2
+print(num1,'+',num2,'=',sum)
+sub=num1-num2
+print(num1,'-',num2,'=',sub)
+mul=num1*num2
+print(num1,'x',num2,'=',mul)
+division=num1/num2
+print(num1,'/',num2,'=',division)
+floor_division=num1//num2
+print('Floor division of',num1,'/',num2,'=',floor_division)
+mod=num1%num2
+print('Remainder of',num1,'/',num2,'=',mod)
+exponent=num1**num2
+print(num1,'raised to',num2,'=',mul)
