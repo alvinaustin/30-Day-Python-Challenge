524eeefbd5e431869419cb631fb672d1b094c647
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-13 21:14:28 +0530

    Add files via upload

---
+++ Day-5/annamariya-day5.py
@@ -0,0 +1,26 @@
+"""
+Author: Anna Mariya Shibu
+Created On : 13/11/2024
+"""
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+import math
+result=math.factorial(num1)
+print(result)
+import math
+result=math.pow(num1,num2)
+print(result)
+import math
+result=math.sqrt(num1)
+print(result)
+import math
+result=math.floor(num1)
+print(result)
+import math
+result=math.ceil(num1)
+print(result)
+ #constants in math module
+import math
+print(math.pi)
+import math
+print(math.e)
