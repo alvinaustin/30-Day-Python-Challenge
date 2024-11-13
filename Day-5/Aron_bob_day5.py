4a04f856a36f542456f39f373d1404e7276c6615
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-13 21:05:16 +0530

    Add files via upload

---
+++ Day-5/Aron_bob_day5.py
@@ -0,0 +1,28 @@
+"""
+Author:Aron Bobby Daniel
+Date:13-11-2024
+Python Programme of importing math module
+"""
+# This will import math module
+import math
+from math import factorial
+
+sqrt=math.sqrt(90)
+print("Square Root of '90'=",sqrt)
+
+power=math.pow(5,8)
+print("Power of 5 raised to 8=",power)
+
+floor=math.floor(4.8)
+print("Largest integer less than or equal to 4.8=",floor)
+
+ceil=math.ceil(4.8)
+print("Largest integer greater than or equal to 4.8=",ceil)
+
+fact=math.factorial(8)
+print("The factorial of 8=",fact)
+
+
+#constants in math module
+print("The Value of pi=",math.pi)
+print("The value of e=",math.e)
