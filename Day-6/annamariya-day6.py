f6b913cd9d5f336660cf642103ef3d37accd334d
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-14 21:32:55 +0530

    Add files via upload

---
+++ Day-6/annamariya-day6.py
@@ -0,0 +1,22 @@
+"""
+Author: Anna Mariya Shibu
+Created On : 14/11/2024
+
+"""
+#stringlength
+string = "Good Morning"
+print(len(string))
+#stringcase
+string= "Good Morning"
+print(string.lower())
+print(string.upper())
+#stringtrimming
+string = " Good Morning "
+print(string.strip())
+#stringreplacement
+string = "Good Morning"
+print(string.replace("Morning","Python"))
+#stringsplitandjoin
+string = "Good Morning"
+print(string.split())
+print(" ".join(string))
