b37c6612b5ba1faad44975c9b31270b8d5fb86cc
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-15 21:31:57 +0530

    Add files via upload

---
+++ Day-7/annamariya-day7.py
@@ -0,0 +1,12 @@
+"""
+Author: Anna Mariya Shibu
+Created On : 15/11/2024
+
+"""
+Grade=int(input("Enter Grade: "))
+if Grade>=90:
+    print("Grade A")
+elif Grade>=80:
+    print("Grade B")
+else:
+    print("Grade C")
