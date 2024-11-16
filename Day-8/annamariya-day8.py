12158ea1695d7fdc1e84de54422cc187dbbd058b
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-16 22:47:41 +0530

    Add files via upload

---
+++ Day-8/annamariya-day8.py
@@ -0,0 +1,23 @@
+"""
+Author: ANNA MARIYA SHIBU
+Created On : 16/11/2024
+
+"""
+#using for loop
+
+for i in range (2,8):
+    print(i)
+
+#using while loop
+
+i=2
+while i<=8:
+    print(i)
+    i=i+2
+#using break and continue
+for i in range (1,8):
+ if i == 6:
+    break
+ if i == 4:
+   continue
+ print(i)
