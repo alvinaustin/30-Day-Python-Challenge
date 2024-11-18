f6e26c6de8186639525a7f9526f6409caf264b37
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-18 23:59:42 +0530

    Add files via upload

---
+++ Day-7/annuday7.py
@@ -0,0 +1,11 @@
+'''
+author name:Annu James
+date:15/11/2024
+'''
+num=int(input("enter a number:"))
+if num>0:
+    print("The number is positive.")
+elif num<0:
+    print("the number is negative.")
+else:
+    print("the number is zero.")
