860cceb8da828b96139e40b664551d7c9b918a0a
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-18 23:46:49 +0530

    Add files via upload

---
+++ Day-6/annuday6.py
@@ -0,0 +1,24 @@
+'''
+author name:Annu James
+date:14/11/2024
+'''
+str="Hello world"
+print(len(str))
+str="Hello,World!"
+print(str.lower())
+str="hello, world!"
+print(str.upper())
+str="hello,world!"
+print(str.title())
+str="Hello ,World!"
+print(str.swapcase())
+str="Hello ,World"
+print(str.capitalize())
+str="    Hello,world!   "
+print(str.strip())
+str="Hello,world!"
+print(str.replace("world","python"))
+str="hello,world!"
+print(str.split(","))
+str="hello","world!"
+print(" ".join(str))  
