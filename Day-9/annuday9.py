048746c2b70fb434cc426572abdb6e26c3b22dbd
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-19 07:26:18 +0530

    Add files via upload

---
+++ Day-9/annuday9.py
@@ -0,0 +1,15 @@
+'''
+author name:Annu James
+date:18/11/24
+'''
+def input(name):
+    print("Good morning" " " + name  +  " " "have a nice day!")  
+input("anna")
+def add_numbers(a,b):
+    return a+b
+result=add_numbers(3,7)
+print("The sum is",result)
+def input(name="guest"):
+    print("Hello! "+name+"Welcome to python")
+input('anna')
+input()
