587c72e2f14f8983f7569da8ea164921469b9777
annujames <annujames2028@cs.sjcetpalai.ac.in>
2024-11-21 23:21:19 +0530

    Add files via upload

---
+++ Day-10/annuday10.py
@@ -0,0 +1,17 @@
+'''
+author name:Annu James
+date:21/11/24
+'''
+fruits=["apple","orange","grapes","mango"]
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+fruits[2]='cherry'
+print(fruits)
+fruits.append("watermelon")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+slice_of_fruits=fruits[1:3]
+print(slice_of_fruits)
