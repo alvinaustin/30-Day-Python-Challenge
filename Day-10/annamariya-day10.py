eddd4947015bc78263b6b3b874a17e09100eed46
ANNA-2006 <annamariyashibu2028@cs.sjcetpalai.ac.in>
2024-11-21 22:55:01 +0530

    Add files via upload

---
+++ Day-10/annamariya-day10.py
@@ -0,0 +1,20 @@
+"""
+Author: ANNA MARIYA SHIBU
+Created On: 21/11/2024
+
+"""
+flowers=["rose","jasmine","lotus","sunflower","lilly"]
+print(flowers)
+print(flowers[-1]),
+print(flowers[-2])
+print(flowers[2])
+flowers[2]="marigold"
+print(flowers)
+flowers.append ("daisy")
+print(flowers)
+flowers.remove ("sunflower")
+print(flowers)
+slice=flowers[1:5]
+print(slice)
+slice=flowers[1:4]
+print(slice)
